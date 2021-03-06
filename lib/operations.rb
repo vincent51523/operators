e952cf1460c86ed09b2a223d1159f284c509e5c7
bin <vincent418@icloud.com>
2021-03-06 12:59:17 -0600

    commit tree b482d2251fa903a7d862d27c18806717a32939c7

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
 def unsafe?(speed)
-
+if speed > 60
+    return true
+end
+if speed < 40
+    return true
+else
+    return false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+    speed > 60 ? true : false || speed < 40 ? true : false
 end
 	
 
